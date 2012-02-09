79cf2e165ea7afc48362b564b83c95f903caa357
Jake McGinty <mcginty@illinois.edu>
2012-02-08 23:53:55 -0600

    css modifications, closer to what we wanted

--- js/flower.js
+++ js/flower.js
@@ -1,32 +1,16 @@
 var links = [
-  {source: "Microsoft", target: "Amazon", type: "licensing"},
-  {source: "Microsoft", target: "HTC", type: "licensing"},
-  {source: "Samsung", target: "Apple", type: "suit"},
-  {source: "Motorola", target: "Apple", type: "suit"},
-  {source: "Nokia", target: "Apple", type: "resolved"},
-  {source: "HTC", target: "Apple", type: "suit"},
-  {source: "Kodak", target: "Apple", type: "suit"},
-  {source: "Microsoft", target: "Barnes & Noble", type: "suit"},
-  {source: "Microsoft", target: "Foxconn", type: "suit"},
-  {source: "Oracle", target: "Google", type: "suit"},
-  {source: "Apple", target: "HTC", type: "suit"},
-  {source: "Microsoft", target: "Inventec", type: "suit"},
-  {source: "Samsung", target: "Kodak", type: "resolved"},
-  {source: "LG", target: "Kodak", type: "resolved"},
-  {source: "RIM", target: "Kodak", type: "suit"},
-  {source: "Sony", target: "LG", type: "suit"},
-  {source: "Kodak", target: "LG", type: "resolved"},
-  {source: "Apple", target: "Nokia", type: "resolved"},
-  {source: "Qualcomm", target: "Nokia", type: "resolved"},
-  {source: "Apple", target: "Motorola", type: "suit"},
-  {source: "Microsoft", target: "Motorola", type: "suit"},
-  {source: "Motorola", target: "Microsoft", type: "suit"},
-  {source: "Huawei", target: "ZTE", type: "suit"},
-  {source: "Ericsson", target: "ZTE", type: "suit"},
-  {source: "Kodak", target: "Samsung", type: "resolved"},
-  {source: "Apple", target: "Samsung", type: "suit"},
-  {source: "Kodak", target: "RIM", type: "suit"},
-  {source: "Nokia", target: "Qualcomm", type: "suit"}
+  {source: "me", target: "RJ", type: "licensing"},
+  {source: "me", target: "Kellie", type: "licensing"},
+  {source: "me", target: "Caroline", type: "suit"},
+  {source: "me", target: "Tim", type: "suit"},
+  {source: "me", target: "Metro", type: "resolved"},
+  {source: "me", target: "Drew", type: "suit"},
+  {source: "me", target: "James", type: "suit"},
+  {source: "me", target: "Brianna", type: "suit"},
+  {source: "me", target: "Roger", type: "suit"},
+  {source: "me", target: "Jeff", type: "suit"},
+  {source: "me", target: "Ralphie", type: "suit"},
+  {source: "me", target: "RoboCop", type: "suit"}
 ];
 
 var nodes = {};
@@ -37,19 +21,19 @@ links.forEach(function(link) {
   link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
 });
 
-var w = 960,
-    h = 500;
+var w = window.innerWidth,
+    h = window.innerHeight;
 
 var force = d3.layout.force()
     .nodes(d3.values(nodes))
     .links(links)
     .size([w, h])
-    .linkDistance(60)
-    .charge(-300)
+    .linkDistance(200)
+    .charge(-1200)
     .on("tick", tick)
     .start();
 
-var svg = d3.select("body").append("svg:svg")
+var svg = d3.select("#container").append("svg:svg")
     .attr("width", w)
     .attr("height", h);
 
@@ -76,7 +60,7 @@ var path = svg.append("svg:g").selectAll("path")
 var circle = svg.append("svg:g").selectAll("circle")
     .data(force.nodes())
   .enter().append("svg:circle")
-    .attr("r", 6)
+    .attr("r", 20)
     .call(force.drag);
 
 var text = svg.append("svg:g").selectAll("g")
